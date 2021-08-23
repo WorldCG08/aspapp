b0025c1f1896c390aed532ce1f3199357cdca095
ymelnyk <yurii.melnyk@dpgmm.net>
2021-08-23 17:27:59 +0300

    ommenting some other view returns from actionResult or ViewResult in controller

--- aspapp/Controllers/MoviesController.cs
+++ aspapp/Controllers/MoviesController.cs
@@ -10,6 +10,16 @@ namespace aspapp.Controllers
         {
             var movie = new Movie() { Name = "Shrek!" };
             return View(movie);
+            // return new ViewResult();
+            // return Content("Hello world!!!");
+            // return HttpNotFound();
+            // return new EmptyResult();
+            // return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
         }
     }
 }
