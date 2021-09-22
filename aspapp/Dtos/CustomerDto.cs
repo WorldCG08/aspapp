6287b815adfa21459bab78dcf3c5d4129cc2180c
ymelnyk <yurii.melnyk@dpgmm.net>
2021-09-22 14:51:52 +0300

    Using IHttpActionResult for setting right requests through API

--- aspapp/Dtos/CustomerDto.cs
+++ aspapp/Dtos/CustomerDto.cs
@@ -7,11 +7,15 @@ namespace aspapp.Dtos
     public class CustomerDto
     {
         public int Id { get; set; }
+        
         [Required]
         public string Name { get; set; }
+        
         public bool IsSubscribedToNewsletter { get; set; }
+        
         public byte MembershipTypeId { get; set; }
-        [Min18YearsOldForMembership]
+        
+        //[Min18YearsOldForMembership]
         public DateTime? Birthdate { get; set; }
     }
 }
